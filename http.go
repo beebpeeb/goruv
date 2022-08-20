dccfe22455105dca0786218863cfe08fe6a0e3b2
Paul Burt <41005722+beebpeeb@users.noreply.github.com>
2022-08-20 14:26:41 +0100

    Fixed missing code in http module

---
+++ http.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+	"time"
+)
+
+func fetchData() (ret Response, err error) {
+	var c = &http.Client{
+		Timeout: time.Second * 10,
+	}
+	res, err := c.Get("https://apis.is/tv/ruv")
+	if err != nil {
+		return
+	}
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return
+	}
+	var data Response
+	jsonError := json.Unmarshal(body, &data)
+	if jsonError != nil {
+		return
+	}
+	return data, err
+}
